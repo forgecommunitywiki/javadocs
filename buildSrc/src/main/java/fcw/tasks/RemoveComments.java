14d9e9d11c200d663c2da71b020371ec2e2b7b77
sciwhiz12 <arnoldnunag12@gmail.com>
2021-02-24 02:47:41 +0800

    Fix RemoveComments task not working properly

--- buildSrc/src/main/java/fcw/tasks/RemoveComments.java
+++ buildSrc/src/main/java/fcw/tasks/RemoveComments.java
@@ -1,43 +1,51 @@
 package fcw.tasks;
 
 import com.github.javaparser.ast.comments.Comment;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JarTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import com.github.javaparser.utils.SourceRoot;
 import fcw.ParserUtils;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.nio.file.Path;
 
 public class RemoveComments extends DefaultTask {
-    @Input public File directory;
+    @Input public File sourcesDir;
+    @Input public Configuration configuration;
     @Input public boolean skipPackageInfo = true;
 
     @TaskAction
     public void act() throws IOException {
-        SourceRoot sourceRoot = new SourceRoot(directory.toPath());
+        SourceRoot sourceRoot = new SourceRoot(sourcesDir.toPath());
+        Path docsRoot = sourcesDir.toPath().toAbsolutePath();
 
-        sourceRoot.getParserConfiguration().setAttributeComments(true);
+        CombinedTypeSolver typeSolver = new CombinedTypeSolver();
+        typeSolver.add(new ReflectionTypeSolver());
+        for (File jar : configuration.getFiles()) {
+            typeSolver.add(new JarTypeSolver(jar));
+        }
+
+        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(typeSolver);
+        sourceRoot.getParserConfiguration()
+            .setSymbolResolver(symbolSolver)
+            .setAttributeComments(true);
         sourceRoot.setPrinter(ParserUtils.PRINTER::print);
 
         sourceRoot.parseParallelized("", (localPath, absolutePath, result) -> {
             if (skipPackageInfo && localPath.getFileName().toString().endsWith("package-info.java")) {
                 return SourceRoot.Callback.Result.DONT_SAVE;
             }
-            return result.getResult()
-                .map(compilationUnit -> {
-                    final List<Comment> comments = compilationUnit.getAllComments().stream()
-                        .filter(Comment::isJavadocComment)
-                        .collect(Collectors.toList());
-                    if (!comments.isEmpty()) {
-                        comments.forEach(Comment::remove);
-                        return SourceRoot.Callback.Result.SAVE;
-                    }
-                    return SourceRoot.Callback.Result.DONT_SAVE;
-                }).orElse(SourceRoot.Callback.Result.DONT_SAVE);
+            result.ifSuccessful(compilationUnit -> compilationUnit.getAllComments().stream()
+                .filter(Comment::isJavadocComment)
+                .forEach(Comment::remove));
+            return SourceRoot.Callback.Result.SAVE;
         });
     }
 }
